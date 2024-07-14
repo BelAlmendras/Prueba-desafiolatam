4c0b89b80f72aa9b06e8ba7f55a38bbfbec91f25
Belen <belen.almendros19@gmail.com>
2024-07-13 20:54:42 -0400

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+}
